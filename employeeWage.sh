d073afe4b584132e96dd1969aab06d541c41baf2
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-04-10 22:06:31 +0530

    Variable Declaration Changed

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,15 +1,15 @@
 echo "welcome to empwage computation";
 
 isPresent=$(( RANDOM  % 3));
+dailyWage=0;
+hoursOfWork=0;
+wagePerHour=20;
 if (( $isPresent == 0)); then
     echo "not present";
 else
     echo "present";
 fi
 
-dailyWage=0;
-hoursOfWork=0;
-wagePerHour=20;
 if (( $isPresent != 0)); then
     hoursOfWork=8;
     dailyWage=$(( $hoursOfWork * $wagePerHour));
